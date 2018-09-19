e58df3f809d550d8a8304d11dc66a61cc825bd19
Stanley Huang <infamous_godhand@yahoo.com>
2018-09-19 09:03:36 +0000

    added validation

--- routes/article.js
+++ routes/article.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { body, check, validationResult } = require('express-validator/check');
 let Article = require("../models/articles");
 
 
@@ -8,22 +9,35 @@ router.get('/', (req, res, next) => {
     res.render('add');
 });
 
-router.post("/", (req, res, next) => {
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-    //res.send("Post route");
-    article.save(err => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        else {
-            req.flash("success", "Article added");
-            res.redirect("/articles");
-        }
-    });
+router.post("/", [
+    check('title').not().isEmpty(),
+    check("author").not().isEmpty(),
+    check("body").not().isEmpty()
+], (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    else {
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+
+        article.save(err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            else {
+                req.flash("success", "Article added");
+                res.redirect("/articles");
+            }
+        });
+    }
 });
+
+
 
 module.exports = router;
